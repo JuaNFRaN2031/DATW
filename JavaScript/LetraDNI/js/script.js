079a6a0478b8653b51bd8d665b1098e45bbb2787
JuaNFRaN2031 <juanfran2000juanfran2000@gmail.com>
2023-06-10 09:19:36 +0200

    .

--- JavaScript/LetraDNI/js/script.js
+++ JavaScript/LetraDNI/js/script.js
@@ -1,8 +1,13 @@
 function calculoLetraDni() {
-    const letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'];
+    const letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E', 'T'];
     let dniSinLetra = document.getElementById("dni").value;
-    let calculoLetra = dniSinLetra % 23;
-    let letra = letras[calculoLetra];
-    document.getElementById("letraDni").innerHTML = "La letra es: " + letra;
-    document.getElementById("dni-completo").innerHTML = "El DNI es: " + dniSinLetra + " - " + letra;
+    if (dniSinLetra == "" || dniSinLetra < 0 || dniSinLetra > 9999999) {
+        let mensajeError = "Indique un número de DNI válido.";
+        document.getElementById("mensajeError").innerHTML = mensajeError;
+    } else {
+        let calculoLetra = dniSinLetra % 23;
+        let letra = letras[calculoLetra];
+        document.getElementById("letraDni").innerHTML = "La letra es: " + letra;
+        document.getElementById("dni-completo").innerHTML = "El DNI completo es: " + dniSinLetra + " - " + letra;
+    }
 };
