daeb1a42d7484e9ff5370de01a059210d4915388
JuaNFRaN2031 <juanfran2000juanfran2000@gmail.com>
2023-06-10 10:00:38 +0200

    .

--- JavaScript/LetraDNI/js/script.js
+++ JavaScript/LetraDNI/js/script.js
@@ -1,7 +1,9 @@
 function calculoLetraDni() {
-    const letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E', 'T'];
+    const letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'];
+
     let dniSinLetra = document.getElementById("dni").value;
-    if (dniSinLetra == "" || dniSinLetra < 0 || dniSinLetra > 9999999) {
+
+    if (dniSinLetra == "" || dniSinLetra <= 0 || dniSinLetra > 99999999) {
         let mensajeError = "Indique un número de DNI válido.";
         document.getElementById("mensajeError").innerHTML = mensajeError;
     } else {
