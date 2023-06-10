2c3230f693fb160587534c91193c4e8600ac884e
JuaNFRaN2031 <juanfran2000juanfran2000@gmail.com>
2023-06-10 12:54:47 +0200

    .

--- JavaScript/cuartoJavaScript/js/script.js
+++ JavaScript/cuartoJavaScript/js/script.js
@@ -1,3 +1,23 @@
-for (let i = 0; i <= 10; i++) {
-    document.getElementById("buclePrueba").innerHTML = i;
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+console.log("--------------------------------------------------");
+function salidaH() {
+    for (let i = 1; i <= 6; i++) {
+        document.write("<h" + i + ">Encabezado de nivel " + i + "</h" + i + ">");
+    }
+}
+
+function calcFact() {
+    let numero = document.getElementById("numero").value;
+    console.log(numero);
+    if (numero == NaN || isNaN(numero)) {
+        document.getElementById("mensajeError").innerHTML = "Indica un número, no letras!!";
+    } else {
+        let resultado = 1;
+        for (let i = 1; i <= numero; i++) {
+            resultado *= i;
+        }
+        document.getElementById("resultadoFact").innerHTML = resultado;
+    }
 }
