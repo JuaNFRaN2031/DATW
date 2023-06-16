14d333078db2ccb0fa67fba8eb2adf69b024cb17
JuaNFRaN2031 <juanfran2000juanfran2000@gmail.com>
2023-06-16 18:57:36 +0200

    .

--- JavaScript/quintoJavaScript/js/script.js
+++ JavaScript/quintoJavaScript/js/script.js
@@ -46,81 +46,44 @@ function calcDay() {
 function calcNota() {
     let nota = parseInt(document.getElementById("nota").value);
     console.log(nota);
-    let notaAulmno;
+    let notaAlumno;
     switch (nota) {
         case 0:
-            notaAulmno = "Insuficiente";
+            notaAlumno = "Insuficiente";
             break;
         case 1:
-            notaAulmno = "Insuficiente";
+            notaAlumno = "Insuficiente";
             break;
         case 2:
-            notaAulmno = "Insuficiente";
+            notaAlumno = "Insuficiente";
             break;
         case 3:
-            notaAulmno = "Insuficiente";
+            notaAlumno = "Insuficiente";
             break;
         case 4:
-            notaAulmno = "Insuficiente";
+            notaAlumno = "Insuficiente";
             break;
         case 5:
-            notaAulmno = "Suficiente";
+            notaAlumno = "Suficiente";
             break;
         case 6:
-            notaAulmno = "Bien";
+            notaAlumno = "Bien";
             break;
         case 7:
-            notaAulmno = "Bien";
+            notaAlumno = "Bien";
             break;
         case 8:
-            notaAulmno = "Notable";
+            notaAlumno = "Notable";
             break;
         case 9:
-            notaAulmno = "Notable";
+            notaAlumno = "Notable";
             break;
         case 10:
-            notaAulmno = "Sobresaliente. Has sacado la mayor nota, ENHORABUENA";
+            notaAlumno = "Sobresaliente. Has sacado la mayor nota, ENHORABUENA";
             break;
         default:
-            notaAulmno = "Esa nota no existe";
+            notaAlumno = "Esa nota no existe";
             break;
     }
-    document.getElementById("notaAulmno").innerHTML = notaAulmno;
-}
-
-/**********************************************************************/
-function crearAlumnos() {
-    var cantidadAlumnos = document.getElementById('cantidadAlumnos').value;
-    var container = document.getElementById('inputsContainer');
-    var outputContainer = document.getElementById('notasContainer');
-
-    // Limpiar los contenedores antes de agregar nuevos elementos
-    container.innerHTML = '';
-    outputContainer.innerHTML = '';
-
-    for (var i = 0; i < cantidadAlumnos; i++) {
-        var input = document.createElement('input');
-        input.type = 'number';
-        input.name = 'alumno' + (i + 1);
-        input.placeholder = 'Alumno ' + (i + 1);
-
-        var output = document.createElement('p');
-        output.id = 'nota' + (i + 1);
-
-        container.appendChild(input);
-        container.appendChild(document.createElement('br')); // Salto de línea
-        outputContainer.appendChild(output);
-    }
-}
-
-function mostrarNotas() {
-    var cantidadAlumnos = document.getElementById('cantidadAlumnos').value;
-    var outputContainer = document.getElementById('notasContainer');
-
-    for (var i = 0; i < cantidadAlumnos; i++) {
-        var input = document.getElementsByName('alumno' + (i + 1))[0];
-        var output = document.getElementById('nota' + (i + 1));
-
-        output.textContent = 'Alumno ' + (i + 1) + ' - Nota: ' + input.value;
-    }
+    document.getElementById("notaAulmno").innerHTML = notaAlumno;
 }
