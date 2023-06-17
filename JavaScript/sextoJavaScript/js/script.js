8ae7dc8baffc5963d5f2452f704e47066033a5e6
JuaNFRaN2031 <juanfran2000juanfran2000@gmail.com>
2023-06-17 10:16:48 +0200

    .

---
+++ JavaScript/sextoJavaScript/js/script.js
@@ -0,0 +1,16 @@
+let num = 0;
+while (num <= 15) {
+    console.log(num);
+    num++;
+}
+
+let interruptor = false;
+while (!interruptor) {
+    let nombre1 = "María", nombre2 = "Juan Fran";
+    if (nombre1 != nombre2) {
+        interruptor = true;
+        console.log(interruptor);
+    }
+    let prueba = console.log(nombre1);
+    prueba
+}
