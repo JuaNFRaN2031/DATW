98f24e747661d1a9d2108ded08b40e4e851cc187
JuaNFRaN2031 <juanfran2000juanfran2000@gmail.com>
2023-06-30 17:30:05 +0200

    .

---
+++ JavaScript/octavoJavaScript/js/script.js
@@ -0,0 +1,47 @@
+function sumar() {
+    let a = 5, b = 3;
+    let suma = a + b;
+    return suma;
+}
+console.log("La primera suma es: " + sumar());
+
+function sumar2() {
+    let suma = a + b;
+    return suma;
+}
+let a = 36, b = 33;
+console.log("La segunda suma es: " + sumar2());
+
+function multiplicar(numero1, numero2) {
+    return numero1 * numero2;
+}
+
+console.log("La primera multiplicación es: " + multiplicar(25, 33));
+console.log("La segunda multiplicación es: " + multiplicar(60, 13));
+console.log("La tercera multiplicación es: " + multiplicar(a, b));
+
+function dividir(numero1, numero2) {
+    return numero2 / numero1;
+}
+console.log("La división es: " + dividir(5, 0));
+
+/** EJERCICIO
+ * Calcula el precio + IVA de los siguientes productos:
+ * leche = 0.76
+ * agua = 1.05
+ * cereales = 2.35
+ * mermelada = 1.96
+ * 
+ * Tomando en cuenta que el IVA es 4%
+ */
+
+let leche = 0.76, agua = 1.05, cereales = 2.35, mermelada = 1.96;
+let iva = 1.04;
+function precioTotal(producto) {
+    return producto * iva;
+}
+
+console.log("El precio de la leche es: " + precioTotal(leche).toFixed(2)+ "€");
+console.log("El precio del agua es: " + precioTotal(agua).toFixed(2)+ "€");
+console.log("El precio de los cereales es: " + precioTotal(cereales).toFixed(2)+ "€");
+console.log("El precio de la mermelada es: " + precioTotal(mermelada).toFixed(2)+ "€");
