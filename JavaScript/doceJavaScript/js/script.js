b5028912f4e26d66035bd96978cea75793e181d1
JuaNFRaN2031 <juanfran2000juanfran2000@gmail.com>
2023-07-13 18:04:27 +0200

    .

---
+++ JavaScript/doceJavaScript/js/script.js
@@ -0,0 +1,11 @@
+window.addEventListener('load', function () {
+
+    var fechaActual = new Date();
+    this.document.querySelector("#fecha").innerHTML = fechaActual.getDate() + "/" + (fechaActual.getMonth() + 1) + "/" + fechaActual.getFullYear();
+
+    var horaActual = new Date();
+    this.document.querySelector("#hora").innerHTML = horaActual.getHours() + ":" + horaActual.getMinutes() + ":" + horaActual.getSeconds();
+
+    var fechaNacimiento = new Date(2002, 10, 8);
+    this.document.querySelector("#fechaNacimiento").innerHTML = fechaNacimiento;
+});
