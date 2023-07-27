6aa732b4444d69ed787ff7c67d9b6d5ef83200a1
JuaNFRaN2031 <juanfran2000juanfran2000@gmail.com>
2023-07-27 17:36:55 +0200

    .

--- JavaScript/treceJavaScript/js/script.js
+++ JavaScript/treceJavaScript/js/script.js
@@ -1,18 +1,21 @@
 window.addEventListener('load', function () {
+    let mensaje = document.querySelector('#mensaje');
+    let msn = "";
     let boton = document.querySelector('.boton');
     boton.addEventListener('click', function () {
         let nombre = document.querySelector('#nombre').value;
         console.log(nombre);
         if (nombre == "" || nombre == null) {
-            document.querySelector('#mensaje').innerHTML = "El nombre no puede estar en blanco";
+            msn = "El nombre no puede estar en blanco";
         }
         let password = document.querySelector('#pass').value;
-        console.log(password);
+        console.log(password.length);
         if (password == "") {
-            document.querySelector('#mensaje2').innerHTML = "La contraseña no puede estar vacía";
+            msn = msn + "<br>La contraseña no puede estar vacía";
         }
         if (8 > password.length > 12 || password > 1) {
-            document.querySelector('#mensaje2').innerHTML = "La contraseña tiene que tener entre 8 y 12 dígitos";
+            msn = msn + "<br>La contraseña tiene que tener entre 8 y 12 dígitos";
         }
+        mensaje.innerHTML = msn;
     });
 });
